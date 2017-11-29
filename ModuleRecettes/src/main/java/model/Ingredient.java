68e3cce2b23d9ff6da86ba52ae61c87f740c7c08
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-11-29 21:09:11 +0100

    ModuleRecette
    Creation classe Ingredient

---
+++ ModuleRecettes/src/main/java/model/Ingredient.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Version;
+
+@Entity
+@SequenceGenerator(name="sequenceIngredient", sequenceName="SequenceIngredient")
+public class Ingredient {
+	
+	@Id @GeneratedValue(generator="sequenceIngredient")
+	@Column(name="id")
+	private Long id;
+	
+	@Version
+	@Column(name="version")
+	private int version;
+	
+	@OneToMany(mappedBy="ingredient")
+	private List<RecetteIngredient> listeRecetteIngredient;
+}
