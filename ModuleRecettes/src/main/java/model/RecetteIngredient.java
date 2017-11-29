58f07acb25262249a91100c6bc1ce3bdca75e481
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-11-29 21:00:27 +0100

    ModuleRecettes
    Création classe intermédiaire RecetteIngredient

---
+++ ModuleRecettes/src/main/java/model/RecetteIngredient.java
@@ -0,0 +1,78 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Version;
+
+@Entity
+@SequenceGenerator(name="sequenceRecetteIngredient",sequenceName="SequenceRecetteIngredient")
+public class RecetteIngredient {
+	
+	@Id @GeneratedValue(generator="sequenceRecetteIngredient")
+	@Column(name="id")
+	private Long id;
+	
+	@Version
+	@Column(name="version")
+	private int version;
+	
+	@ManyToOne
+	@JoinColumn(name="recette_id")
+	private Recette recette;
+	
+	@ManyToOne
+	@JoinColumn(name="ingredient_id")
+	private Ingredient ingredient;
+	
+	@Column(name="quantite")
+	private String quantite;
+
+	public RecetteIngredient() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public Recette getRecette() {
+		return recette;
+	}
+
+	public void setRecette(Recette recette) {
+		this.recette = recette;
+	}
+
+	public Ingredient getIngredient() {
+		return ingredient;
+	}
+
+	public void setIngredient(Ingredient ingredient) {
+		this.ingredient = ingredient;
+	}
+
+	public String getQuantite() {
+		return quantite;
+	}
+
+	public void setQuantite(String quantite) {
+		this.quantite = quantite;
+	}
+}
