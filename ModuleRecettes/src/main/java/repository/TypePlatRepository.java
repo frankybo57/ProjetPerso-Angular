74dcfc218a83217b3d8d5e6f9796bcd4709a3a24
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-11-29 21:23:47 +0100

    ModuleRecette
    Ajout des repos

---
+++ ModuleRecettes/src/main/java/repository/TypePlatRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.TypePlat;
+
+public interface TypePlatRepository extends JpaRepository<TypePlat, Short> {
+
+}
