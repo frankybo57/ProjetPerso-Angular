f2ea1bafd3c6b4bf81f31515c25b365e406299c2
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-12-05 21:43:04 +0100

    correctif

--- ModuleRecettes/src/main/java/modulerecettes/repository/TypeIngredientRepository.java
+++ ModuleRecettes/src/main/java/modulerecettes/repository/TypeIngredientRepository.java
@@ -1,10 +1,15 @@
 package modulerecettes.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import modulerecettes.model.TypeIngredient;
 
 
 public interface TypeIngredientRepository extends JpaRepository<TypeIngredient, Short>{
-
+	@Query("from TypeIngredient ti where ti.typePere = :typePere")
+	public List<TypeIngredient> findAllTypeIngredientFils(@Param("typePere") TypeIngredient typePere);
 }
