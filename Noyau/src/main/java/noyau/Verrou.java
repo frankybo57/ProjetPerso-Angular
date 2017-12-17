77b8809334e7ffbd4af6c63cda8e1d515a308235
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2017-12-17 10:28:16 +0100

    Hashage

--- Noyau/src/main/java/noyau/Verrou.java
+++ Noyau/src/main/java/noyau/Verrou.java
@@ -1,10 +1,16 @@
 package noyau;
 
-import noyau.model.Utilisateur;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public abstract class Verrou {
 	
-	private static final int increment = 16;
+	final static Logger logger = LogManager.getLogger(Verrou.class);
+	
 	private static final String sel = "sel";
 	
 	
@@ -13,7 +19,7 @@ public abstract class Verrou {
 	}
 	
 	public static String cryptage(String password) {
-		return fonctionCryptage(password);
+		return sha512(password,sel);
 	}
 	
 	private static String fonctionCryptage(String password) {
@@ -33,9 +39,40 @@ public abstract class Verrou {
 		return temp.toString();
 	}
 	
+	private static String sha512(String password, String salt){
+		String generatedPassword = null;
+	    try {
+	         MessageDigest md = MessageDigest.getInstance("SHA-512");
+	         md.update(salt.getBytes("UTF-8"));
+	         byte[] bytes = md.digest(password.getBytes("UTF-8"));
+	         StringBuilder sb = new StringBuilder();
+	         for(int i=0; i< bytes.length ;i++){
+	            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+	         }
+	         generatedPassword = sb.toString();
+	    } 
+	    catch (NoSuchAlgorithmException e) {
+	    	if(logger.isErrorEnabled()) {
+	    		logger.error("Erreur lors de la tentative de hashage : le hashage SHA-512 n'est pas disponible dans l'environnement");
+	    	}
+	    	if(logger.isDebugEnabled()) {
+	    		logger.debug(e);
+	    	}
+	    }
+	    catch (UnsupportedEncodingException e){
+	    	if(logger.isErrorEnabled()) {
+	    		logger.error("Erreur lors de la tentative de hashage : l'encodage en UTF-8 n'est pas supporté");
+	    	}
+	    	if(logger.isDebugEnabled()) {
+	    		logger.debug(e);
+	    	}
+	    }
+	    
+	    return generatedPassword;
+	}
+	
 	private static String md5(String password) {
 		return password;
-		
 	}
 	
 	private static String fonctionCryptageOld(String password) {
