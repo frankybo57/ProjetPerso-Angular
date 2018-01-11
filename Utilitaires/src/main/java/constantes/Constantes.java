0a0f838730a88129c6a1af69db22f7fe666a77e4
frankybo57 <32872685+frankybo57@users.noreply.github.com>
2018-01-11 19:52:39 +0100

    Ajout du module Utilitaires

---
+++ Utilitaires/src/main/java/constantes/Constantes.java
@@ -0,0 +1,26 @@
+package constantes;
+
+public class Constantes {
+	
+	public static final String UTILISATEUR_NON_TROUVE = "Utilisateur non trouvé.";
+	public static final String NOUVEL_UTILISATEUR_AVEC_ID ="Problème : le nouvel utilisateur ne devrait pas avoir d'id.";
+	public static final String UTILISATEUR_SANS_ID ="Problème : l'utilisateur devrait avoir un id.";
+	
+	public static final String RECETTE_NON_TROUVEE = "Recette non trouvée.";
+	public static final String NOUVELLE_RECETTE_AVEC_ID ="Problème : la nouvelle recette ne devrait pas avoir d'id.";
+	public static final String RECETTE_SANS_ID ="Problème : la recette devrait avoir un id.";
+	
+	public static final String INGREDIENT_NON_TROUVE = "Ingrédient non trouvé.";
+	public static final String NOUVEL_INGREDIENT_AVEC_ID ="Problème : le nouvel ingrédient ne devrait pas avoir d'id.";
+	public static final String INGREDIENT_SANS_ID ="Problème : l'ingrédient devrait avoir un id.";
+	
+	public static final String TYPEPLAT_NON_TROUVE = "TypePlat non trouvé.";
+	public static final String NOUVEAU_TYPEPLAT_AVEC_ID ="Problème : le nouveau typePlat ne devrait pas avoir d'id.";
+	public static final String TYPEPLAT_SANS_ID ="Problème : le typePlat devrait avoir un id.";
+	
+	private Constantes() {
+		
+	}
+	
+	
+}
